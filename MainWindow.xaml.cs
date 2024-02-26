53ef3a856bf2698bd9ff6cc2540ab3832c525edd
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-02-26 17:29:16 +0530

    Implemented for various shapes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,64 +8,107 @@
 // ----------------------------------------------------------------------------------------
 
 using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Forms;
 using System.IO;
 using System.Linq;
-using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
 
-#region ScribblePad ---------------------------------------------------------------------------------------- 
-namespace ScribblePad {
+namespace DrawingShapes {
+   #region Program ---------------------------------------------------------------------------------------- 
    public partial class MainWindow : Window {
       public MainWindow () {
          InitializeComponent ();
          MouseDown += OnMouseDown;
-         MouseUp += OnMouseUp;
          MouseMove += OnMouseMove;
+         MouseUp += OnMouseUp;
       }
 
-      #region Events ----------------------------------------
-      /// <summary>Changes the colour of the pen</summary>
-      void ChangeColor () {
-         if (!IsErase) {
-            ColorDialog dialog = new ();
-            if (dialog.ShowDialog () == System.Windows.Forms.DialogResult.OK)
-               mPen.Brush = new SolidColorBrush (Color.FromArgb (dialog.Color.A, dialog.Color.R, dialog.Color.G, dialog.Color.B));
+      #region Click Events ----------------------------------------
+      /// <summary>Click event for circle button</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnCircle (object sender, RoutedEventArgs e) => mSelectedShape = "Circle";
+
+      /// <summary>Click event for ellipse button</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnEllipse (object sender, RoutedEventArgs e) => mSelectedShape = "Ellipse";
+
+      /// <summary>Click event for line button</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnLine (object sender, RoutedEventArgs e) => mSelectedShape = "Line";
+
+      /// <summary>Click event for rectangle button</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnRect (object sender, RoutedEventArgs e) => mSelectedShape = "Rectangle";
+
+      /// <summary>Click event for redo button</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnReDo (object sender, RoutedEventArgs e) {
+         if (mStack.Count > 0) {
+            mShapes.Add (mStack.Pop ());
+            InvalidateVisual ();
          }
       }
 
-      /// <summary>Event handler for the button to change colour</summary>
+      /// <summary>Click event for pen button</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnColorFillClick (object sender, RoutedEventArgs e) => ChangeColor ();
+      void OnScribble (object sender, RoutedEventArgs e) => mSelectedShape = "Scribble";
 
-      /// <summary>Event handler to erase the drawing</summary>
+      /// <summary>Click event for undo button</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnEraserClick (object sender, RoutedEventArgs e) { mPen.Brush = Brushes.Black; mPen.Thickness = 15; IsErase = true; }
+      void OnUnDo (object sender, RoutedEventArgs e) {
+         if (mShapes.Count > 0) {
+            mStack.Push (mShapes.Last ());
+            mShapes.Remove (mShapes.Last ());
+            InvalidateVisual ();
+         }
+      }
+      #endregion
 
       #region Mouse Events ----------------------------------------
       /// <summary>Event handler for mouse down event</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnMouseDown (object sender, MouseButtonEventArgs e) {
-         if (e.ButtonState == MouseButtonState.Pressed) {
-            mStart = e.GetPosition (this);
-            mPoints.Add (mStart);
-            mCount++;
-            InvalidateVisual ();
+      void OnMouseDown (object sender, MouseEventArgs e) {
+         if (mSelectedShape != null) {
+            mStartPoint = e.GetPosition (this);
+            switch (mSelectedShape) {
+               case "Rectangle":
+                  mCurrentShape = new Rectangle (mStartPoint);
+                  break;
+               case "Ellipse":
+                  mCurrentShape = new Ellipse (mStartPoint);
+                  break;
+               case "Circle":
+                  mCurrentShape = new Circle (mStartPoint);
+                  break;
+               case "Line":
+                  mCurrentShape = new Line (mStartPoint);
+                  break;
+               case "Scribble":
+                  mCurrentShape = new Scribble (mStartPoint);
+                  break;
+               default: break;
+            }
+            IsDrawing = true;
          }
       }
 
       /// <summary>Event handler for mouse move event</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnMouseMove (object sender, System.Windows.Input.MouseEventArgs e) {
-         if (e.LeftButton == MouseButtonState.Pressed) {
-            mEnd = e.GetPosition (this);
-            mPoints.Add (mEnd);
+      void OnMouseMove (object sender, MouseEventArgs e) {
+         if (IsDrawing) {
+            mEndPoint = e.GetPosition (this);
+            mCurrentShape.Update (mEndPoint);
             InvalidateVisual ();
          }
       }
@@ -73,200 +116,116 @@ namespace ScribblePad {
       /// <summary>Event handler for mouse up event</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnMouseUp (object sender, MouseButtonEventArgs e) {
-         if (e.LeftButton == MouseButtonState.Released) {
-            var brush = mPen.Brush;
-            double thickness = mPen.Thickness;
-            mScribbles.Add ((mCount, brush, thickness, mPoints.ToList ()));
-            mPoints.Clear ();
+      void OnMouseUp (object sender, MouseEventArgs e) {
+         if (IsDrawing) {
+            mEndPoint = e.GetPosition (this);
+            mCurrentShape.Update (mEndPoint);
+            mShapes.Add (mCurrentShape);
+            mCurrentShape = null;
+            IsDrawing = false;
+            InvalidateVisual ();
          }
       }
       #endregion
 
-      #region Open ----------------------------------------
-      /// <summary>Event handler for open button click event</summary>
+      #region Menu Items(New, Save, Open) ----------------------------------------
+      /// <summary>Click event of new menu item</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnOpenClick (object sender, RoutedEventArgs e) {
-         OpenFileDialog dlgBox = new ();
-         dlgBox.Title = "Select a file";
-         dlgBox.Filter = "Text File|*.txt|Binary File|*.bin";
-         if (dlgBox.ShowDialog () == System.Windows.Forms.DialogResult.OK)
-            switch (dlgBox.FilterIndex) {
-               case 1: OpenText (dlgBox.FileName); break;
-               case 2: OpenBinary (dlgBox.FileName); break;
-            }
-      }
-
-      /// <summary>Loads the text file</summary>
-      /// <param name="path">Path of the selected file</param>
-      void OpenText (string path) {
-         var lines = File.ReadAllLines (path);
-         List<Point> points = new ();
-         int totalCount = int.Parse (lines[0]);
-         for (int i = 1; i <= totalCount; i++) {
-            int index = Array.IndexOf (lines, $"List{i}");
-            var brush = (Brush)new BrushConverter ().ConvertFromString (lines[index + 1]);
-            double thickness = double.Parse (lines[index + 2]);
-            (int count, int k) = (int.Parse (lines[index + 3]), Array.IndexOf (lines, lines[index + 3]));
-            for (int j = 1; j <= count; j++) {
-               string[] coords = lines[k + j].Split (',');
-               Point p = new ();
-               p.X = double.Parse (coords[0]);
-               p.Y = double.Parse (coords[1]);
-               points.Add (p);
-            }
-            mScribbles.Add ((i, brush, thickness, points.ToList ()));
-            points.Clear ();
-         }
+      void OnNew (object sender, RoutedEventArgs e) {
+         mShapes.Clear ();
          InvalidateVisual ();
       }
 
-      /// <summary>Loads the binary file</summary>
-      /// <param name="path">Path of the selected binary file</param>
-      void OpenBinary (string path) {
-         using (BinaryReader reader = new (File.Open (path, FileMode.Open))) {
-            Point p = new ();
-            List<Point> points = new ();
-            int totalCount = reader.ReadInt32 ();
-            for (int i = 1; i <= totalCount; i++) {
-               int count = reader.ReadInt32 ();
-               byte a = reader.ReadByte ();
-               byte b = reader.ReadByte ();
-               byte r = reader.ReadByte ();
-               byte g = reader.ReadByte ();
-               var brush = new SolidColorBrush (Color.FromArgb (a, r, g, b));
-               double thickness = reader.ReadDouble ();
-               for (int j = 0; j < count; j++) {
-                  p.X = reader.ReadDouble ();
-                  p.Y = reader.ReadDouble ();
-                  points.Add (p);
-               }
-               mScribbles.Add ((i, brush, thickness, points.ToList ())); points.Clear ();
+      /// <summary>Click event of open menu item</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnOpen (object sender, RoutedEventArgs e) {
+         OpenFileDialog dlgBox = new ();
+         dlgBox.Title = "Select a file";
+         dlgBox.Filter = "Binary File|*.bin";
+         if (dlgBox.ShowDialog () == System.Windows.Forms.DialogResult.OK)
+            using (BinaryReader reader = new (File.Open (dlgBox.FileName, FileMode.Open))) {
+               int totalCount = reader.ReadInt32 ();
+               for (int i = 0; i < totalCount; i++)
+                  switch (reader.ReadInt32 ()) { // Reads the file based on the shape (1- rectangle, 2- ellipse, 3- circle, 4- line, 5- scribble)
+                     case 1:
+                        var rect = new Rectangle ();
+                        rect.Open (reader);
+                        mShapes.Add (rect);
+                        InvalidateVisual ();
+                        break;
+                     case 2:
+                        var ellipse = new Ellipse ();
+                        ellipse.Open (reader);
+                        mShapes.Add (ellipse);
+                        InvalidateVisual ();
+                        break;
+                     case 3:
+                        var circle = new Circle ();
+                        circle.Open (reader);
+                        mShapes.Add (circle);
+                        InvalidateVisual ();
+                        break;
+                     case 4:
+                        var line = new Line ();
+                        line.Open (reader);
+                        mShapes.Add (line);
+                        InvalidateVisual ();
+                        break;
+                     case 5:
+                        var scribble = new Scribble ();
+                        scribble.Open (reader);
+                        mShapes.Add (scribble);
+                        InvalidateVisual ();
+                        break;
+                     default: break;
+                  }
+
+
             }
-            InvalidateVisual ();
-         }
       }
-      #endregion
 
-      /// <summary>Event handler to switch to pen</summary>
+      /// <summary>Click event for save menu item</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnPenClick (object sender, RoutedEventArgs e) {
-         if (IsErase) {
-            for (int i = mScribbles.Count - 1; i >= 0; i--) {
-               if (mScribbles[i].color == Brushes.Black) continue;
-               mPen.Brush = mScribbles[i].color;
-               mPen.Thickness = mScribbles[i].thickness;
-               break;
-            }
-            IsErase = false;
-         }
-      }
-
-      /// <summary>Re-renders the deleted drawing</summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      void OnReDo (object sender, RoutedEventArgs e) {
-         if (mStack.Count > 0) {
-            mScribbles.Add (mStack.Pop ());
-            InvalidateVisual ();
-         }
-      }
-
-      /// <summary>Overriding on-render method for main window</summary> 
-      /// <param name="dc"></param>
-      protected override void OnRender (DrawingContext dc) {
-         base.OnRender (dc);
-         foreach (var (_, color, thickness, points) in mScribbles)
-            for (int i = 1; i < points.Count; i++)
-               dc.DrawLine (new Pen (color, thickness), points[i - 1], points[i]);
-         for (int i = 1; i < mPoints.Count; i++)
-            dc.DrawLine (new Pen (mPen.Brush, mPen.Thickness), mPoints[i - 1], mPoints[i]);
-      }
-
-      /// <summary>Deletes the last drawing</summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      void OnUnDo (object sender, RoutedEventArgs e) {
-         if (mScribbles.Count > 0) {
-            mStack.Push (mScribbles.Last ());
-            mScribbles.Remove (mScribbles.Last ());
-            InvalidateVisual ();
-         }
-      }
-
-      #region Save ----------------------------------------
-      /// <summary>Event handler for save button click event</summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      void OnSaveClick (object sender, RoutedEventArgs e) {
+      void OnSave (object sender, RoutedEventArgs e) {
          SaveFileDialog dlgBox = new ();
          dlgBox.FileName = "Drawing";
-         dlgBox.Filter = "Text File|*.txt|Binary File|*.bin";
+         dlgBox.Filter = "Binary File|*.bin";
          DialogResult dr = dlgBox.ShowDialog ();
          if (dr == System.Windows.Forms.DialogResult.OK) {
             var pathToFile = dlgBox.FileName;
-            switch (dlgBox.FilterIndex) {
-               case 1: SaveText (pathToFile); break;
-               case 2: SaveBinary (pathToFile); break;
-            }
-         }
-      }
-
-      /// <summary>Saves scribble pad as text file</summary>
-      /// <param name="path">Path of the file to be saved</param>
-      void SaveText (string path) {
-         using (TextWriter writer = new StreamWriter (path)) {
-            writer.WriteLine (mScribbles.Count); // Total number of scribbles
-            int count = 1;
-            foreach (var (_, color, thickness, points) in mScribbles) {
-               writer.WriteLine ($"List{count}");
-               writer.WriteLine (color.ToString ());
-               writer.WriteLine ($"{thickness}");
-               writer.WriteLine (points.Count);
-               for (int i = 0; i < points.Count; i++)
-                  writer.WriteLine (points[i]);
-               count++;
-            }
-         }
-      }
-
-      /// <summary>Saves scribble pad as binary file</summary>
-      /// <param name="path">Path of the file to be saved</param>
-      void SaveBinary (string path) {
-         using (BinaryWriter writer = new (File.Open (path, FileMode.Create))) {
-            writer.Write (mScribbles.Count); // Total number of scribbles
-            foreach (var (_, color, thickness, points) in mScribbles) {
-               writer.Write (points.Count); // Number of points of a scribble
-               var brush = color;
-               var colour = ((SolidColorBrush)brush).Color;
-               byte a = colour.A; byte b = colour.B; byte r = colour.R; byte g = colour.G; // Gets the ABRG values of the color
-               writer.Write (a);
-               writer.Write (b);
-               writer.Write (r);
-               writer.Write (g);
-               writer.Write (thickness);
-               for (int j = 0; j < points.Count; j++) {
-                  writer.Write (points[j].X);
-                  writer.Write (points[j].Y);
-               }
+            using (BinaryWriter writer = new (File.Open (pathToFile, FileMode.Create))) {
+               writer.Write (mShapes.Count); // Total number of shapes
+               foreach (var shape in mShapes)
+                  shape.Save (writer);
             }
          }
       }
       #endregion
 
-      #endregion
+      /// <summary>Overriding on-render method</summary>
+      /// <param name="dc"></param>
+      protected override void OnRender (DrawingContext dc) {
+         base.OnRender (dc);
+         foreach (var shape in mShapes) {
+            shape.Draw (dc);
+         }
+         if (IsDrawing && mCurrentShape != null) {
+            mCurrentShape.Draw (dc);
+         }
+      }
 
       #region Private data ------------------------------------------
-      List<(int count, Brush color, double thickness, List<Point> points)> mScribbles = new ();
-      Stack<(int count, Brush color, double thickness, List<Point> points)> mStack = new ();
-      Pen mPen = new (Brushes.White, 2);
-      List<Point> mPoints = new ();
-      Point mStart, mEnd = new ();
-      bool IsErase = false;
-      int mCount;
+      List<Shape> mShapes = new (); // Collection of Shapes
+      Stack<Shape> mStack = new (); // Used for undo and redo operations
+      Point mStartPoint;
+      Point mEndPoint;
+      Shape mCurrentShape;         // Shape which is being rendered
+      bool IsDrawing = false;
+      string mSelectedShape;       // The selected shape as string
       #endregion
    }
+   #endregion
 }
-#endregion
---
+++ Shape.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.IO;
+
+namespace DrawingShapes {
+   public abstract class Shape {        // Base class
+      public Shape () { }
+
+      public Shape (Point startPoint) => mStartPoint = startPoint;
+
+
+      public abstract void Draw (DrawingContext dc);    // Renders the shape
+
+      public abstract void Open (BinaryReader reader);    // Opens and loads binary file
+
+      public abstract void Save (BinaryWriter writer);    // Saves the drawing in binary format
+
+      public abstract void Update (Point endpoint);    // Updates the state of the shape
+
+      protected Point mStartPoint;    // Starting point of the shape
+   }
+
+   class Rectangle : Shape {        // Rectangle class
+      public Rectangle () { mRect = new Rect (); }
+
+      public Rectangle (Point startPoint) : base (startPoint) => mRect = new Rect (startPoint, startPoint);
+
+      public override void Update (Point endPoint) {
+         mEndPoint = endPoint;
+         mRect = new Rect (mStartPoint, mEndPoint);
+      }
+
+      public override void Draw (DrawingContext dc) =>
+         dc.DrawRectangle (null, new Pen (Brushes.White, 2), mRect);
+
+      public override void Save (BinaryWriter writer) {
+         writer.Write (1);    // Used for identification of rectangle from the binary file
+         writer.Write (mStartPoint.X);
+         writer.Write (mStartPoint.Y);
+         writer.Write (mEndPoint.X);
+         writer.Write (mEndPoint.Y);
+      }
+
+      public override void Open (BinaryReader reader) {
+         mStartPoint.X = reader.ReadDouble ();
+         mStartPoint.Y = reader.ReadDouble ();
+         mEndPoint.X = reader.ReadDouble ();
+         mEndPoint.Y = reader.ReadDouble ();
+         mRect = new Rect (mStartPoint, mEndPoint);
+      }
+
+      Rect mRect;
+      Point mEndPoint;
+   }
+
+   class Ellipse : Shape {        // Ellipse class
+      public Ellipse () => mEllipse = new EllipseGeometry ();
+
+      public Ellipse (Point startPoint) : base (startPoint) => mEllipse = new EllipseGeometry (startPoint, 0, 0);
+
+      public override void Update (Point endPoint) {
+         radiusX = Math.Abs (endPoint.X - mStartPoint.X) / 2;
+         radiusY = Math.Abs (endPoint.Y - mStartPoint.Y) / 2;
+         center = new (mStartPoint.X + radiusX, mStartPoint.Y + radiusY);
+         mEllipse = new EllipseGeometry (center, radiusX, radiusY);
+      }
+
+      public override void Draw (DrawingContext dc) =>
+         dc.DrawGeometry (null, new Pen (Brushes.White, 2), mEllipse);
+
+      public override void Save (BinaryWriter writer) {
+         writer.Write (2);    // Used for identification of ellipse in binary file
+         writer.Write (radiusX); writer.Write (radiusY);
+         writer.Write (center.X);
+         writer.Write (center.Y);
+      }
+
+      public override void Open (BinaryReader reader) {
+         radiusX = reader.ReadDouble ();
+         radiusY = reader.ReadDouble ();
+         center.X = reader.ReadDouble ();
+         center.Y = reader.ReadDouble ();
+         mEllipse = new EllipseGeometry (center, radiusX, radiusY);
+      }
+
+      EllipseGeometry mEllipse;
+      double radiusX, radiusY;
+      Point center;
+   }
+
+   class Circle : Shape {        // Circle class
+      public Circle () { }
+
+      public Circle (Point startPoint) : base (startPoint) => mCircle = new EllipseGeometry (startPoint, 0, 0);
+
+      public override void Update (Point endPoint) {
+         radius = Math.Abs (endPoint.X - mStartPoint.X) / 2;
+         center = new (mStartPoint.X + radius, mStartPoint.Y + radius);
+         mCircle = new EllipseGeometry (center, radius, radius);
+      }
+
+      public override void Draw (DrawingContext dc) =>
+         dc.DrawGeometry (null, new Pen (Brushes.White, 2), mCircle);
+
+      public override void Save (BinaryWriter writer) {
+         writer.Write (3);    // Used for identification of circle in binary file
+         writer.Write (center.X);
+         writer.Write (center.Y);
+         writer.Write (radius);
+      }
+
+      public override void Open (BinaryReader reader) {
+         center.X = reader.ReadDouble ();
+         center.Y = reader.ReadDouble ();
+         radius = reader.ReadDouble ();
+         mCircle = new EllipseGeometry (center, radius, radius);
+      }
+
+      EllipseGeometry mCircle;
+      double radius;
+      Point center;
+   }
+
+   class Line : Shape {       // Line class
+      public Line () { }
+
+      public Line (Point startPoint) : base (startPoint) => mStartPoint = startPoint;
+
+      public override void Update (Point endPoint) => mEndpoint = endPoint;
+
+      public override void Draw (DrawingContext dc) {
+         dc.DrawLine (new Pen (Brushes.White, 2), mStartPoint, mEndpoint);
+      }
+
+      public override void Save (BinaryWriter writer) {
+         writer.Write (4);    // Used for identification of line in binary file
+         writer.Write (mStartPoint.X);
+         writer.Write (mStartPoint.Y);
+         writer.Write (mEndpoint.X);
+         writer.Write (mEndpoint.Y);
+      }
+
+      public override void Open (BinaryReader reader) {
+         mStartPoint.X = reader.ReadDouble ();
+         mStartPoint.Y = reader.ReadDouble ();
+         mEndpoint.X = reader.ReadDouble ();
+         mEndpoint.Y = reader.ReadDouble ();
+      }
+
+      Point mEndpoint;
+   }
+
+   class Scribble : Shape {        // Scribble class
+      public Scribble () { }
+
+      public Scribble (Point startPoint) : base (startPoint) => mPoints.Add (startPoint);
+
+      public override void Update (Point endPoint) => mPoints.Add (endPoint);
+
+      public override void Draw (DrawingContext dc) {
+         for (int i = 1; i < mPoints.Count; i++)
+            dc.DrawLine (new Pen (Brushes.White, 2), mPoints[i - 1], mPoints[i]);
+      }
+
+      public override void Save (BinaryWriter writer) {
+         writer.Write (5);    // Used for identification of scribble in binary file
+         writer.Write (mPoints.Count);
+         foreach (var point in mPoints) {
+            writer.Write (point.X);
+            writer.Write (point.Y);
+         }
+      }
+
+      public override void Open (BinaryReader reader) {
+         int count = reader.ReadInt32 ();
+         for (int i = 0; i < count; i++) {
+            Point p = new ();
+            p.X = reader.ReadDouble ();
+            p.Y = reader.ReadDouble ();
+            mPoints.Add (p);
+         }
+      }
+
+      List<Point> mPoints = new ();    // List of points of scribble
+   }
+}
