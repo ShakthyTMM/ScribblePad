9c913a619eb5b604cbbf105c1f4918df3db39c52
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-03-04 12:48:46 +0530

    Added connected lines shape

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,22 +13,23 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Media;
+using System.Windows.Input;
+using System.ComponentModel;
 using MouseEventArgs = System.Windows.Input.MouseEventArgs;
 using MessageBox = System.Windows.Forms.MessageBox;
-using System.ComponentModel;
 
 namespace DrawingShapes {
-   #region Program ---------------------------------------------------------------------------------------- 
+   #region MainWindow ---------------------------------------------------------------------------------------- 
    public partial class MainWindow : Window {
       public MainWindow () {
          InitializeComponent ();
          MouseDown += OnMouseDown;
          MouseMove += OnMouseMove;
-         MouseUp += OnMouseUp;
+         EnableChanged (Tools.ReDo, false);    // Initially sets the 'IsEnabled' property of button as false
+         EnableChanged (Tools.UnDo, false);    // and image opacity
       }
 
       #region Tools ----------------------------------------
-
       /// <summary>Click event for color change button</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
@@ -48,12 +49,11 @@ namespace DrawingShapes {
       /// <param name="e"></param>
       void OnReDo (object sender, RoutedEventArgs e) {
          if (mStack.Count > 0) {
-            if (mCount != mShapes.Count) mStack.Clear ();
-            else {
-               mShapes.Add (mStack.Pop ());
-               mCount = mShapes.Count;
-               InvalidateVisual ();
-            }
+            mShapes.Add (mStack.Pop ());
+            mCount = mShapes.Count;
+            if (mCount > 0) EnableChanged (Tools.UnDo, true);
+            if (mStack.Count == 0) EnableChanged (Tools.ReDo, false);
+            InvalidateVisual ();
          }
       }
 
@@ -63,10 +63,20 @@ namespace DrawingShapes {
       void OnUnDo (object sender, RoutedEventArgs e) {
          if (mShapes.Count > 0) {
             mStack.Push (mShapes.Last ());
+            EnableChanged (Tools.ReDo, true);
             mShapes.Remove (mShapes.Last ());
             mCount = mShapes.Count;
+            if (mCount == 0) EnableChanged (Tools.UnDo, false);
             InvalidateVisual ();
          }
+      }
+
+      // Changes the 'IsEnabled' property of undo and redo buttons
+      void EnableChanged (Tools tool, bool IsEnabled) {
+         if (tool == Tools.ReDo)
+            if (IsEnabled) { mRedo.IsEnabled = true; mRI.Opacity = 1; } else { mRedo.IsEnabled = false; mRI.Opacity = 0.2; }
+         if (tool == Tools.UnDo)
+            if (IsEnabled) { mUndo.IsEnabled = true; mUI.Opacity = 1; } else { mUndo.IsEnabled = false; mUI.Opacity = 0.2; }
       }
 
       /// <summary>Click event for pen button</summary>
@@ -81,6 +91,8 @@ namespace DrawingShapes {
       /// <param name="sender"></param>
       /// <param name="e"></param>
       void OnCircle (object sender, RoutedEventArgs e) => mSelectedShape = Shapes.Circle;
+
+      void OnConnectedLine (object sender, RoutedEventArgs e) => mSelectedShape = Shapes.ConnectedLine;
 
       /// <summary>Click event for ellipse button</summary>
       /// <param name="sender"></param>
@@ -102,57 +114,58 @@ namespace DrawingShapes {
       /// <summary>Event handler for mouse down event</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
-      void OnMouseDown (object sender, MouseEventArgs e) {
-         mStartPoint = e.GetPosition (this);
-         switch (mSelectedShape) {
-            case Shapes.Rectangle:
-               mCurrentShape = new Rectangle (mStartPoint);
-               break;
-            case Shapes.Ellipse:
-               mCurrentShape = new Ellipse (mStartPoint);
-               break;
-            case Shapes.Circle:
-               mCurrentShape = new Circle (mStartPoint);
-               break;
-            case Shapes.Line:
-               mCurrentShape = new Line (mStartPoint);
-               break;
-            case Shapes.Scribble:
-               mCurrentShape = new Scribble (mStartPoint);
-               break;
-            case Shapes.Eraser:
-               mCurrentShape = new Eraser (mStartPoint);
-               break;
-            default: break;
+      void OnMouseDown (object sender, MouseButtonEventArgs e) {
+         if (e.RightButton == MouseButtonState.Released) {
+            if (mCurrentShape is ConnectedLine) { UpdateShape (e); mCurrentShape.AddLines (mEndPoint); } else {
+               if (IsDrawing) AddToList (e);
+               else {
+                  mStartPoint = e.GetPosition (this);
+                  switch (mSelectedShape) {
+                     case Shapes.Rectangle:
+                        mCurrentShape = new Rectangle (mStartPoint);
+                        break;
+                     case Shapes.Ellipse:
+                        mCurrentShape = new Ellipse (mStartPoint);
+                        break;
+                     case Shapes.Circle:
+                        mCurrentShape = new Circle (mStartPoint);
+                        break;
+                     case Shapes.Line:
+                        mCurrentShape = new CustomLine (mStartPoint);
+                        break;
+                     case Shapes.Scribble:
+                        mCurrentShape = new Scribble (mStartPoint);
+                        break;
+                     case Shapes.Eraser:
+                        mCurrentShape = new Eraser (mStartPoint);
+                        break;
+                     case Shapes.ConnectedLine:
+                        mCurrentShape = new ConnectedLine (mStartPoint);
+                        break;
+                     default: break;
+                  }
+                  IsDrawing = true;
+               }
+            }
          }
-         IsDrawing = true;
       }
+
+      // Routine to get end point and update the shape
+      void UpdateShape (MouseEventArgs e) {
+         mEndPoint = e.GetPosition (this);
+         mCurrentShape.Update (mEndPoint);
+      }
+
+      // Adds the shapes to the list
+      void AddToList (MouseEventArgs e) { UpdateShape (e); AddShapes (); }
 
       /// <summary>Event handler for mouse move event</summary>
       /// <param name="sender"></param>
       /// <param name="e"></param>
       void OnMouseMove (object sender, MouseEventArgs e) {
-         if (IsDrawing) {
-            mEndPoint = e.GetPosition (this);
-            mCurrentShape.Update (mEndPoint);
-            InvalidateVisual ();
-         }
+         if (IsDrawing) { UpdateShape (e); InvalidateVisual (); }
       }
 
-      /// <summary>Event handler for mouse up event</summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      void OnMouseUp (object sender, MouseEventArgs e) {
-         if (IsDrawing) {
-            mEndPoint = e.GetPosition (this);
-            mCurrentShape.Update (mEndPoint);
-            var brush = mPen.Brush;
-            mShapes.Add ((mCurrentShape, brush));
-            mCurrentShape = null;
-            IsDrawing = false;
-            InvalidateVisual ();
-         }
-      }
       #endregion
 
       #region Menu Items ----------------------------------------
@@ -160,8 +173,10 @@ namespace DrawingShapes {
       /// <param name="sender"></param>
       /// <param name="e"></param>
       void OnExit (object sender, RoutedEventArgs e) {
-         if (!IsSaved && mShapes.Count != 0) Prompt (sender, e);
-         else Close ();
+         if (!IsSaved && mShapes.Count != 0) {
+            Prompt (sender, e);
+            if (!IsCancelled) Close ();
+         } else Close ();
       }
 
       /// <summary>Click event of new menu item</summary>
@@ -177,6 +192,7 @@ namespace DrawingShapes {
 
       // Prompts the user to save the drawing
       void Prompt (object sender, RoutedEventArgs e) {
+         IsCancelled = false;
          DialogResult dr = MessageBox.Show ("Do you want to save changes to Untitled?", "ScribblePad", MessageBoxButtons.YesNoCancel);
          switch (dr) {
             case System.Windows.Forms.DialogResult.Yes:
@@ -202,28 +218,26 @@ namespace DrawingShapes {
          dlgBox.Filter = "Binary File|*.bin";
          if (dlgBox.ShowDialog () == System.Windows.Forms.DialogResult.OK) {
             if (mShapes.Count != 0 && !IsSaved) Prompt (sender, e);
-            if (IsSaved) {
-               mShapes.Clear ();
-               InvalidateVisual ();
-            }
+            if (IsSaved) { mShapes.Clear (); InvalidateVisual (); }
             if (!IsCancelled)
                using (BinaryReader reader = new (File.Open (dlgBox.FileName, FileMode.Open))) {
-                  IsSaved= true;
+                  IsSaved = true;
                   pathToFile = dlgBox.FileName;
                   int totalCount = reader.ReadInt32 ();
                   for (int i = 0; i < totalCount; i++) {
                      byte a = reader.ReadByte (); byte b = reader.ReadByte (); byte r = reader.ReadByte (); byte g = reader.ReadByte ();
                      var brush = new SolidColorBrush (Color.FromArgb (a, r, g, b));
-                     switch (reader.ReadInt32 ()) { // Reads the file based on the shape (1- rectangle, 2- ellipse, 3- circle, 4- line, 5- scribble, 6- eraser)
-                        case 1: Read (reader, new Rectangle ()); break;
-                        case 2: Read (reader, new Ellipse ()); break;
-                        case 3: Read (reader, new Circle ()); break;
-                        case 4: Read (reader, new Line ()); break;
-                        case 5: Read (reader, new Scribble ()); break;
-                        case 6: Read (reader, new Eraser ()); break;
+                     switch (reader.ReadInt32 ()) { // Reads the file based on the shape (0- rectangle, 1- ellipse, 2- circle, 3- line, 4- scribble, 5- eraser, 6-connected lines)
+                        case 0: Read (reader, new Rectangle ()); break;
+                        case 1: Read (reader, new Ellipse ()); break;
+                        case 2: Read (reader, new Circle ()); break;
+                        case 3: Read (reader, new CustomLine ()); break;
+                        case 4: Read (reader, new Scribble ()); break;
+                        case 5: Read (reader, new Eraser ()); break;
+                        case 6: Read (reader, new ConnectedLine ()); break;
                      }
 
-                     void Read (BinaryReader reader, Shape shape) {
+                     void Read (BinaryReader reader, Shape shape) {    // Routine to read the shapes
                         shape.Open (reader);
                         mShapes.Add ((shape, brush));
                         InvalidateVisual ();
@@ -252,28 +266,45 @@ namespace DrawingShapes {
          } else dr = dlgBox.ShowDialog ();
          if (dr == System.Windows.Forms.DialogResult.OK) {
             IsSaved = true;
-            IsCancelled = false;
+            pathToFile = dlgBox.FileName;
             using (BinaryWriter writer = new (File.Open (dlgBox.FileName, FileMode.Create))) {
                writer.Write (mShapes.Count); // Total number of shapes
                foreach (var shape in mShapes) {
-                  var brush = shape.Item2;
+                  var brush = shape.brush;
                   var color = ((SolidColorBrush)brush).Color;
                   byte a = color.A; byte b = color.B; byte r = color.R; byte g = color.G; // Gets the ABRG values of the color
                   writer.Write (a); writer.Write (b); writer.Write (r); writer.Write (g);
-                  shape.Item1.Save (writer);
+                  shape.shape.Save (writer);
                }
             }
          }
       }
       #endregion
 
+      /// <summary>Event handler for escape key press</summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void OnKeyDown (object sender, System.Windows.Input.KeyEventArgs e) {
+         if (e.Key == Key.Escape) { mCurrentShape.AddLines (mEndPoint); AddShapes (); }
+      }
+
+      // Routine to add shapes to the list
+      void AddShapes () {
+         var brush = mPen.Brush;
+         mShapes.Add ((mCurrentShape, brush));
+         if (mCount != mShapes.Count) { mStack.Clear (); EnableChanged (Tools.ReDo, false); }
+         EnableChanged (Tools.UnDo, true);
+         mCurrentShape = null;
+         IsDrawing = false;
+         InvalidateVisual ();
+      }
+
       /// <summary>Overriding on-closing method of main window</summary>
       /// <param name="e"></param>
       protected override void OnClosing (CancelEventArgs e) {
          if (!IsSaved && mShapes.Count != 0) {
             Prompt (null, null);
-            if (IsCancelled) e.Cancel = true;
-            if(!IsSaved) base.OnClosing (e);
+            if (!IsCancelled) base.OnClosing (e); else e.Cancel = true;
          } else base.OnClosing (e);
       }
 
@@ -282,13 +313,13 @@ namespace DrawingShapes {
       protected override void OnRender (DrawingContext dc) {
          base.OnRender (dc);
          foreach (var shape in mShapes)
-            shape.Item1.Draw (dc, shape.Item2);
+            shape.shape.Draw (dc, shape.brush);
          if (IsDrawing && mCurrentShape != null)
             mCurrentShape.Draw (dc, mPen.Brush);
       }
 
       #region Private data ------------------------------------------
-      List<(Shape, Brush)> mShapes = new ();    // Collection of Shapes and their corresponding color
+      List<(Shape shape, Brush brush)> mShapes = new ();    // Collection of Shapes and their corresponding color
       Stack<(Shape, Brush)> mStack = new ();    // Used for undo and redo operations
       Pen mPen = new (Brushes.White, 2);
       Point mStartPoint, mEndPoint;
@@ -298,8 +329,9 @@ namespace DrawingShapes {
       bool IsCancelled = false;    // True, if user clicks 'cancel' to save the drawing
       string pathToFile;           // Path of the saved file
       int mCount;                  // Holds count of the shapes after undo operation
-      Shapes mSelectedShape = Shapes.Scribble;    // Sets default shape as scribble
-      enum Shapes { Rectangle, Ellipse, Line, Circle, Scribble, Eraser }
+      Shapes mSelectedShape = Shapes.Line;    // Sets default shape as line
+      enum Tools { UnDo, ReDo }
+      enum Shapes { Rectangle, Ellipse, Line, Circle, Scribble, Eraser, ConnectedLine }
       #endregion
    }
    #endregion
--- Shape.cs
+++ Shape.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 using System.IO;
+using System.Windows.Shapes;
 
 namespace DrawingShapes {
+   #region Shape ----------------------------------------------------------------------------------------
    public abstract class Shape {        // Base class
       public Shape () { }
 
       public Shape (Point startPoint) => mStartPoint = startPoint;
+
+      public virtual void AddLines (Point startPoint) { }    // Collects the lines of connected lines
 
       public abstract void Draw (DrawingContext dc, Brush brush);    // Renders the shape
 
@@ -19,8 +23,11 @@ namespace DrawingShapes {
       public abstract void Update (Point endpoint);    // Updates the state of the shape
 
       protected Point mStartPoint;    // Starting point of the shape
+
+      protected enum Shapes { Rectangle, Ellipse, Circle, CustomLine, Scribble, Eraser, ConnectedLine }    // Identifies the shapes
    }
 
+   #region Rectangle ----------------------------------------
    class Rectangle : Shape {        // Rectangle class
       public Rectangle () { mRect = new Rect (); }
 
@@ -30,19 +37,14 @@ namespace DrawingShapes {
          dc.DrawRectangle (null, new Pen (brush, 2), mRect);
 
       public override void Open (BinaryReader reader) {
-         mStartPoint.X = reader.ReadDouble (); 
-         mStartPoint.Y = reader.ReadDouble ();
-         mEndPoint.X = reader.ReadDouble (); 
-         mEndPoint.Y = reader.ReadDouble ();
+         (mStartPoint.X, mStartPoint.Y, mEndPoint.X, mEndPoint.Y) = (reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble ());
          mRect = new Rect (mStartPoint, mEndPoint);
       }
 
       public override void Save (BinaryWriter writer) {
-         writer.Write (1);    // Used for identification of rectangle from the binary file
-         writer.Write (mStartPoint.X); 
-         writer.Write (mStartPoint.Y);
-         writer.Write (mEndPoint.X); 
-         writer.Write (mEndPoint.Y);
+         writer.Write ((int)Shapes.Rectangle);    // Used for identification of rectangle from the binary file
+         writer.Write (mStartPoint.X); writer.Write (mStartPoint.Y);
+         writer.Write (mEndPoint.X); writer.Write (mEndPoint.Y);
       }
 
       public override void Update (Point endPoint) {
@@ -53,7 +55,9 @@ namespace DrawingShapes {
       Rect mRect;
       Point mEndPoint;
    }
+   #endregion
 
+   #region Ellipse ----------------------------------------
    class Ellipse : Shape {        // Ellipse class
       public Ellipse () => mEllipse = new EllipseGeometry ();
 
@@ -63,31 +67,27 @@ namespace DrawingShapes {
          dc.DrawGeometry (null, new Pen (brush, 2), mEllipse);
 
       public override void Open (BinaryReader reader) {
-         radiusX = reader.ReadDouble (); 
-         radiusY = reader.ReadDouble ();
-         mStartPoint.X = reader.ReadDouble (); 
-         mStartPoint.Y = reader.ReadDouble ();
+         (radiusX, radiusY, mStartPoint.X, mStartPoint.Y) = (reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble ());
          mEllipse = new EllipseGeometry (mStartPoint, radiusX, radiusY);
       }
 
       public override void Save (BinaryWriter writer) {
-         writer.Write (2);    // Used for identification of ellipse in binary file
-         writer.Write (radiusX); 
-         writer.Write (radiusY);
-         writer.Write (mStartPoint.X); 
-         writer.Write (mStartPoint.Y);
+         writer.Write ((int)Shapes.Ellipse);    // Used for identification of ellipse in binary file
+         writer.Write (radiusX); writer.Write (radiusY);
+         writer.Write (mStartPoint.X); writer.Write (mStartPoint.Y);
       }
 
       public override void Update (Point endPoint) {
-         radiusX = Math.Abs (endPoint.X - mStartPoint.X) / 2;
-         radiusY = Math.Abs (endPoint.Y - mStartPoint.Y) / 2;
+         (radiusX, radiusY) = (Math.Abs (endPoint.X - mStartPoint.X) / 2, Math.Abs (endPoint.Y - mStartPoint.Y) / 2);
          mEllipse = new EllipseGeometry (mStartPoint, radiusX, radiusY);
       }
 
       EllipseGeometry mEllipse;
       double radiusX, radiusY;
    }
+   #endregion
 
+   #region Circle ----------------------------------------
    class Circle : Shape {        // Circle class
       public Circle () { }
 
@@ -97,16 +97,13 @@ namespace DrawingShapes {
          dc.DrawGeometry (null, new Pen (brush, 2), mCircle);
 
       public override void Open (BinaryReader reader) {
-         mStartPoint.X = reader.ReadDouble (); 
-         mStartPoint.Y = reader.ReadDouble ();
-         radius = reader.ReadDouble ();
+         (mStartPoint.X, mStartPoint.Y, radius) = (reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble ());
          mCircle = new EllipseGeometry (mStartPoint, radius, radius);
       }
 
       public override void Save (BinaryWriter writer) {
-         writer.Write (3);    // Used for identification of circle in binary file
-         writer.Write (mStartPoint.X); writer.Write (mStartPoint.Y);
-         writer.Write (radius);
+         writer.Write ((int)Shapes.Circle);    // Used for identification of circle in binary file
+         writer.Write (mStartPoint.X); writer.Write (mStartPoint.Y); writer.Write (radius);
       }
 
       public override void Update (Point endPoint) {
@@ -117,35 +114,33 @@ namespace DrawingShapes {
       EllipseGeometry mCircle;
       double radius;
    }
+   #endregion
 
-   class Line : Shape {       // Line class
-      public Line () { }
+   #region CustomLine ----------------------------------------
+   class CustomLine : Shape {       // Line class
+      public CustomLine () { }
 
-      public Line (Point startPoint) : base (startPoint) => mStartPoint = startPoint;
+      public CustomLine (Point startPoint) : base (startPoint) => mStartPoint = startPoint;
 
       public override void Draw (DrawingContext dc, Brush brush) =>
         dc.DrawLine (new Pen (brush, 2), mStartPoint, mEndpoint);
 
-      public override void Open (BinaryReader reader) {
-         mStartPoint.X = reader.ReadDouble ();
-         mStartPoint.Y = reader.ReadDouble ();
-         mEndpoint.X = reader.ReadDouble ();
-         mEndpoint.Y = reader.ReadDouble ();
-      }
+      public override void Open (BinaryReader reader) =>
+         (mStartPoint.X, mStartPoint.Y, mEndpoint.X, mEndpoint.Y) = (reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble ());
 
       public override void Save (BinaryWriter writer) {
-         writer.Write (4);    // Used for identification of line in binary file
-         writer.Write (mStartPoint.X);
-         writer.Write (mStartPoint.Y);
-         writer.Write (mEndpoint.X);
-         writer.Write (mEndpoint.Y);
+         writer.Write ((int)Shapes.CustomLine);    // Used for identification of line in binary file
+         writer.Write (mStartPoint.X); writer.Write (mStartPoint.Y);
+         writer.Write (mEndpoint.X); writer.Write (mEndpoint.Y);
       }
 
       public override void Update (Point endPoint) => mEndpoint = endPoint;
 
       Point mEndpoint;
    }
+   #endregion
 
+   #region Scribble ----------------------------------------
    class Scribble : Shape {        // Scribble class
       public Scribble () { }
 
@@ -160,18 +155,16 @@ namespace DrawingShapes {
          int count = reader.ReadInt32 ();
          for (int i = 0; i < count; i++) {
             Point p = new ();
-            p.X = reader.ReadDouble ();
-            p.Y = reader.ReadDouble ();
+            (p.X, p.Y) = (reader.ReadDouble (), reader.ReadDouble ());
             mPoints.Add (p);
          }
       }
 
       public override void Save (BinaryWriter writer) {
-         writer.Write (5);    // Used for identification of scribble in binary file
+         writer.Write ((int)Shapes.Scribble);    // Used for identification of scribble in binary file
          writer.Write (mPoints.Count);
          foreach (var point in mPoints) {
-            writer.Write (point.X);
-            writer.Write (point.Y);
+            writer.Write (point.X); writer.Write (point.Y);
          }
       }
 
@@ -179,7 +172,9 @@ namespace DrawingShapes {
 
       List<Point> mPoints = new ();    // List of points of scribble
    }
+   #endregion
 
+   #region Eraser ----------------------------------------
    class Eraser : Shape {
       public Eraser () { }
 
@@ -194,18 +189,16 @@ namespace DrawingShapes {
          int count = reader.ReadInt32 ();
          for (int i = 0; i < count; i++) {
             Point p = new ();
-            p.X = reader.ReadDouble ();
-            p.Y = reader.ReadDouble ();
+            (p.X, p.Y) = (reader.ReadDouble (), reader.ReadDouble ());
             mPoints.Add (p);
          }
       }
 
       public override void Save (BinaryWriter writer) {
-         writer.Write (6);    // Used for identification of scribble in binary file
+         writer.Write ((int)Shapes.Eraser);    // Used for identification of scribble in binary file
          writer.Write (mPoints.Count);
          foreach (var point in mPoints) {
-            writer.Write (point.X);
-            writer.Write (point.Y);
+            writer.Write (point.X); writer.Write (point.Y);
          }
       }
 
@@ -213,4 +206,51 @@ namespace DrawingShapes {
 
       List<Point> mPoints = new ();
    }
+   #endregion
+
+   #region ConnectedLine ----------------------------------------
+   class ConnectedLine : Shape {
+      public ConnectedLine () { }
+
+      public ConnectedLine (Point startPoint) : base (startPoint) => mStartPoint = startPoint;
+
+      public override void Draw (DrawingContext dc, Brush brush) {
+         foreach (var line in mConnectedLines)
+            dc.DrawLine (new Pen (brush, 2), new Point (line.X1, line.Y1), new Point (line.X2, line.Y2));
+         dc.DrawLine (new Pen (brush, 2), mStartPoint, mEndpoint);
+      }
+
+      public override void Open (BinaryReader reader) {
+         int count = reader.ReadInt32 ();
+         for (int i = 0; i < count; i++) {
+            Line line = new ();
+            (line.X1, line.Y1, line.X2, line.Y2) = (reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble (), reader.ReadDouble ());
+            mConnectedLines.Add (line);
+         }
+      }
+
+      public override void Save (BinaryWriter writer) {
+         writer.Write ((int)Shapes.ConnectedLine);    // Used for identification of line in binary file
+         writer.Write (mConnectedLines.Count);
+         foreach (var line in mConnectedLines) {
+            writer.Write (line.X1); writer.Write (line.Y1);
+            writer.Write (line.X2); writer.Write (line.Y2);
+         }
+      }
+
+      public override void Update (Point endPoint) => mEndpoint = endPoint;
+
+      public override void AddLines (Point endPoint) {    // Adds the lines to the list
+         Line line = new ();
+         (line.X1, line.Y1, line.X2, line.Y2) = (mStartPoint.X, mStartPoint.Y, endPoint.X, endPoint.Y);
+         mConnectedLines.Add (line);
+         mStartPoint = endPoint;
+      }
+
+      Point mEndpoint;
+      List<Line> mConnectedLines = new ();    // List of lines 
+   }
+   #endregion
+
+   #endregion
 }
